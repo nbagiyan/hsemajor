50ee9f17cf4165f66c118e5d58e4582c9ffc5726
Nerses Bagiyan <nkb997@gmail.com>
2017-01-24 22:20:32 +0300

    factory.cpp

---
+++ seminars/sem1_patterns/factory_pattern/factory.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+  SoldierFactory* fact = new SoldierFactory;
+  TankFactory* fact_tank = new TankFactory;
+
+  //std::
+  vector<Unit*> units;
+
+  units.push_back(fact->createUnit());
+  units.push_back(fact_tank->createUnit());
+
+  // units.push_back(Unit::createUnit(Soldier_ID));
+  // units.push_back(Unit::createUnit(Artillery_ID));
+  // units.push_back(Unit::createUnit(Tank_ID));
+
+  for (int i = 0; i < units.size(); i++)
+    units[i]->info();
+}
