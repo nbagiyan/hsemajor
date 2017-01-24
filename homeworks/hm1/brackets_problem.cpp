515fa34d4960dea80adf6fdc8f9bc2905e8f06af
Nerses Bagiyan <nkb997@gmail.com>
2017-01-24 22:55:42 +0300

    bracket_problem
    Check if brackets' structure is correct

---
+++ homeworks/hm1/brackets_problem.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+
+using namespace std;
+
+int check(string& str);
+
+int check(string& str) {
+	int cnt = 0;
+	for (string::iterator it = str.begin(); it != str.end(); ++it) {
+
+		if ((*it == '(') || (*it == '{') || (*it == '[')) {
+			cnt++;
+		}
+		if ((*it == ')') || (*it == '}') || (*it == ']')) {
+			cnt--;
+		}
+		if (cnt < 0) {
+			return 0;
+		}
+	}
+	if (cnt > 0) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+
+int main() {
+	string str;
+	getline(cin, str);
+	if (check(str)) {
+		cout << "Brackets' structure is correct";
+	}
+	else {
+		cout << "Brackets' structure is incorrect";
+	}
+	return 0;
+}
